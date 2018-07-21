9936580b618a17ed7afb34a12875381d11f6aeb5
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-07-20 20:52:39 -0500

    Add files via upload

---
+++ Java/JavaMPeak.java
@@ -0,0 +1,40 @@
+/*
+Kevin Nguyen
+Basic Java program
+
+How to run (linux):
+	1.) open command line and cd into directory folder of where this code is
+	2.) type in "javac *.java" to compile java files
+	3.) type in "java JavaMPeak"
+*/
+
+//imports
+import java.io.*;
+import java.util.*;
+
+//class
+class JavaMPeak {
+	//Global Declarations
+
+	//main driver
+	public static void main(String[] args) {
+		//local Declarations
+		System.out.println("HelloWorld");
+	}
+
+	//peakIndexInMountainArray will output the peak which is the ith index
+	public int peakIndexInMountainArray(int[] A) {
+        //local declarations
+        int i = 0, length = A.length, result = 0;
+        
+        //iterate over all items
+        while(i < length) {
+            if((i > 0) && (A[i-1] < A[i]) && (A[i] > A[i+1])) {
+                result = i;
+            }
+            i++;
+        }
+        
+        return result;
+    }	
+}
